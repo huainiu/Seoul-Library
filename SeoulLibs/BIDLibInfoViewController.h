005f36c7c6e5e7a37426643cc27735cdd1bb7158
Mark <mhkim318@gmail.com>
2012-08-21 22:11:51 +0900

    석우가 고친거
    평점, 댓글 업데이트 반영

--- SeoulLibs/BIDLibInfoViewController.h
+++ SeoulLibs/BIDLibInfoViewController.h
@@ -10,11 +10,18 @@
 
 @interface BIDLibInfoViewController : UIViewController <UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate>
 {
+    UIScrollView *scrollView;
     UILabel *ratingLabel;
+    UITextField *ratingText;
+    UITextField *commentField;//댓글 입력창
+    UITableView *commentTable; //댓글 표시되는 테이블
 }
+
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UITextField *commentField;
+@property (nonatomic, retain) IBOutlet UITextField *commentField; //댓글 입력창
 @property (nonatomic, retain) IBOutlet UILabel *ratingLabel;
+@property (nonatomic, retain) IBOutlet UITextField *ratingText;
+@property (nonatomic, retain) IBOutlet UITableView *commentTable;
 
 - (IBAction)textFieldDoneEdit; // 배경을 탭하면 키보드 사라지는 액션
 - (IBAction)goToMap; // 지도보기 누르면 지도 뷰 컨트롤러로 이동
@@ -22,6 +29,20 @@
 
 - (void) getRating:(NSString *)library_class idx:(NSString *)idx; //평점 가져오기
 - (void) parseRating:(NSString *)jsonString; //평점 파싱
+- (void) getComment:(NSString *)library_class idx:(NSString *)idx; //댓글 가져오기
+- (void) updateComment:(NSString *)library idx:(NSString *)idx article:(NSString *)article uuid:(NSString *)uuid; //댓글 등록하기
+- (void) getRating:(NSString *)library_class idx:(NSString *)idx; //평점 가져오기
+- (void) updateRating:(NSString *)library idx:(NSString *)idx rating:(NSString *)rating uuid:(NSString *)uuid ; //평점 업데이트
+
+- (void) parseComment:(NSString *)jsonString; //댓글 파싱
+- (void) parseUpdateComment:(NSString *)jsonString; //댓글 업데이트 후 받아온 데이터 파싱
+- (void) parseRating:(NSString *)jsonString; //평점 파싱
+- (void) parseUpdateRating:(NSString *)jsonString; //평점 업데이트 후 받아온 데이터 파싱
+
+- (IBAction)rate:(id)sender; //평점 매기기
+
+- (IBAction)registerComment:(id)sender; //댓글 등록
+
 
 
 @end
--- SeoulLibs/BIDLibInfoViewController.m
+++ SeoulLibs/BIDLibInfoViewController.m
@@ -10,6 +10,7 @@
 #import "BIDFirstViewController.h"
 #import "BIDMapViewController.h"
 #import "HTTPRequest.h"
+#import "HTTPRequestPost.h"
 #import "SBJson.h"
 
 
@@ -18,13 +19,15 @@
 @end
 
 NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해주는 flag
+NSString *libInfoDataFlag2 = nil; //어떤 데이터를 받아온건지 구분해주는 flag
 
 @implementation BIDLibInfoViewController
 
 @synthesize scrollView = _scrollView;
 @synthesize commentField;
 @synthesize ratingLabel;
-
+@synthesize ratingText;
+@synthesize commentTable;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -38,20 +41,12 @@ NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    switch ([[NSUserDefaults standardUserDefaults] integerForKey:@"tabFlag"]) {
-        case 1:
-            self.title = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"1_lib%i_name", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]];
-            [self getRating:[[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"1_lib%i_class", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]] idx:[[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"1_lib%i_id", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]]];
-
-            break;
-            
-        case 3:
-            self.title = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"3_lib%i_name", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]];
-            [self getRating:[[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"3_lib%i_class", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]] idx:[[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"3_lib%i_id", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]]];
-
-            break;
-    }
     
+    [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"commentCount"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    self.title = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"currentLibInfo_name", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]];
+                
     //스크롤뷰 생성
     _scrollView.frame = CGRectMake(0, 0, 320, 320);
     _scrollView.contentSize = CGSizeMake(320,800);
@@ -70,19 +65,26 @@ NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해
 
 
 - (void)viewDidAppear:(BOOL)animated {
+
+    NSLog(@"LibInfoViewController - viewDidAppear 메서드 실행");
+
+    ratingLabel.text = [[NSUserDefaults standardUserDefaults] stringForKey:@"averageRating"];
     
-    switch ([[NSUserDefaults standardUserDefaults] integerForKey:@"tabFlag"]) {
-        case 1:
-            self.title = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"1_lib%i_name", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]];
-
-            break;
-            
-        case 3:
-            self.title = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"3_lib%i_name", [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedLib"]]];
-
-            break;
-    }
+    [self getRating:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_class"] idx:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_id"]];
+    
+    [self getComment:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_class"] idx:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_id"]];
 }
+
+
+//댓글 등록
+- (IBAction)registerComment:(id)sender {
+    
+    [self updateComment:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_class"] idx:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_id"] article:commentField.text uuid:[[NSUserDefaults standardUserDefaults] stringForKey:@"uuid"]];
+    
+    //updateComment:(NSString *)library idx:(NSString *)idx article:(NSString *)article uuid:(NSString *)uuid
+    
+}
+
 
 
 
@@ -90,7 +92,7 @@ NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해
 - (void) getRating:(NSString *)library_class idx:(NSString *)idx { 
     NSLog(@"getRating 메서드 실행");
     
-    libInfoDataFlag = @"getRating";
+    libInfoDataFlag2 = @"getRating";
     
     //접속할 주소 설정
     //예시 : http://seoullibrary.herokuapp.com/rating/small/1
@@ -103,7 +105,7 @@ NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해
     NSDictionary *bodyObject = [NSDictionary dictionaryWithObjectsAndKeys: nil];
     
     // 통신 완료 후 호출할 델리게이트 셀렉터 설정
-    [httpRequest setDelegate:self selector:@selector(didReceiveFinished:)];
+    [httpRequest setDelegate:self selector:@selector(didReceiveFinished2:)];
     
     // 페이지 호출
     [httpRequest requestUrl:url bodyObject:bodyObject];  
@@ -112,23 +114,129 @@ NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해
 
 //평점 파싱
 - (void) parseRating:(NSString *)jsonString {    
+    NSLog(@"BIDLibInfoViewController - parseRating 메서드 실행");
+
     SBJsonParser* parser = [ [ SBJsonParser alloc ] init ];
     
     NSMutableDictionary* rating = [ parser objectWithString: jsonString ];    
     
     NSLog(@"요청처리시간: %@", [rating valueForKey:@"time"]);
-    NSLog(@"반경검색 결과의 수: %@", [rating valueForKey:@"total_rows"]);
+    NSLog(@"결과의 수: %@", [rating valueForKey:@"total_rows"]);
     
     NSArray *rowsArray = [ rating objectForKey:@"rows"];
     
     for (int i=0; i < [rowsArray count]; i++) {
         NSLog(@"도서관 id: %@", [[rowsArray objectAtIndex:i] valueForKey:@"cartodb_id"]);
         NSLog(@"해당 도서관의 평점 평균: %@", [[rowsArray objectAtIndex:i] valueForKey:@"average"]);
-        [[NSUserDefaults standardUserDefaults] setValue:[rowsArray objectAtIndex:i] forKey:@"averageRating"];
+        [[NSUserDefaults standardUserDefaults] setValue:[[rowsArray objectAtIndex:i] valueForKey:@"average"] forKey:@"averageRating"];
         [[NSUserDefaults standardUserDefaults] synchronize];
     }
     ratingLabel.text = [[NSUserDefaults standardUserDefaults] stringForKey:@"averageRating"];
 }
+
+
+//댓글 가져오기. GET
+- (void) getComment:(NSString *)library_class idx:(NSString *)idx { 
+    NSLog(@"getComment 메서드 실행");
+    
+    libInfoDataFlag = @"getComment";
+    
+    //접속할 주소 설정
+    //예시 : http://seoullibrary.herokuapp.com/comment/small/1
+    NSString *url = [[NSString alloc] initWithFormat:@"http://seoullibrary.herokuapp.com/comment/%@/%@", library_class, idx];
+    
+    // HTTP Request 인스턴스 생성
+    HTTPRequest *httpRequest = [[HTTPRequest alloc] init];
+    
+    // POST로 전송할 데이터 설정
+    NSDictionary *bodyObject = [NSDictionary dictionaryWithObjectsAndKeys: nil];
+    
+    // 통신 완료 후 호출할 델리게이트 셀렉터 설정
+    [httpRequest setDelegate:self selector:@selector(didReceiveFinished:)];
+    
+    // 페이지 호출
+    [httpRequest requestUrl:url bodyObject:bodyObject];
+}
+
+
+//행정구역 파싱
+- (void) parseComment:(NSString *)jsonString {
+    NSLog(@"BIDLibInfoViewController - parseComment 메서드 실행");
+
+    SBJsonParser* parser = [ [ SBJsonParser alloc ] init ];
+    
+    NSMutableDictionary* comment = [ parser objectWithString: jsonString ];    
+    
+    NSLog(@"요청처리시간: %@", [comment valueForKey:@"time"]);
+    NSLog(@"댓글요청 결과의 수: %@", [comment valueForKey:@"total_rows"]);
+    
+    NSArray *rowsArray = [ comment objectForKey:@"rows"];
+    [[NSUserDefaults standardUserDefaults] setInteger:[rowsArray count] forKey:@"commentCount"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    for (int i=0; i < [rowsArray count]; i++) {
+        NSLog(@"해당 댓글의 id%i: %@", i, [[rowsArray objectAtIndex:i] valueForKey:@"comment_id"]);
+        NSLog(@"도서관 id%i: %@", i,  [[rowsArray objectAtIndex:i] valueForKey:@"cartodb_id"]);
+        NSLog(@"해당 댓글의 본문%i: %@", i, [[rowsArray objectAtIndex:i] valueForKey:@"comment_article"]);
+        [[NSUserDefaults standardUserDefaults] setValue:[[rowsArray objectAtIndex:i] valueForKey:@"comment_article"] forKey:[NSString stringWithFormat:@"comment%i_article", i]];
+        NSLog(@"댓글을 남긴 기기의 uuid%i: %@", i, [[rowsArray objectAtIndex:i] valueForKey:@"comment_uuid"]);
+        NSLog(@"코멘트를 남긴 시각%i: %@", i, [[rowsArray objectAtIndex:i] valueForKey:@"comment_date"]);
+        [[NSUserDefaults standardUserDefaults] setValue:[[rowsArray objectAtIndex:i] valueForKey:@"comment_date"] forKey:[NSString stringWithFormat:@"comment%i_date", i]];
+        
+        [commentTable beginUpdates];
+        NSIndexPath *indexPath0 = [NSIndexPath indexPathForRow:i inSection:0];
+        
+        NSArray *indexPathArray = [NSArray arrayWithObjects:indexPath0, nil];
+        
+        [commentTable reloadRowsAtIndexPaths:indexPathArray withRowAnimation:UITableViewRowAnimationFade];
+        
+        [commentTable endUpdates];
+    }
+    
+    
+    
+}
+
+
+
+//댓글 업데이트. POST형식
+- (void) updateComment:(NSString *)library idx:(NSString *)idx article:(NSString *)article uuid:(NSString *)uuid { 
+    NSLog(@"updateComment 메서드 실행");
+    
+    libInfoDataFlag = @"updateComment";
+    
+    //접속할 주소 설정
+    NSString *url = [[NSString alloc] initWithFormat:@"http://seoullibrary.herokuapp.com/comment/update"];
+    
+    // HTTP Request 인스턴스 생성
+    HTTPRequestPost *httpRequestPost = [[HTTPRequestPost alloc] init];
+    
+    // POST로 전송할 데이터 설정
+    NSDictionary *bodyObject = [NSDictionary dictionaryWithObjectsAndKeys:library,@"library",idx, @"idx", article, @"article", uuid, @"uuid", nil];
+    
+    // 통신 완료 후 호출할 델리게이트 셀렉터 설정
+    [httpRequestPost setDelegate:self selector:@selector(didReceiveFinished:)];
+    
+    // 페이지 호출
+    [httpRequestPost requestUrl:url bodyObject:bodyObject];
+}
+
+
+//댓글 업데이트 후 받아온 데이터 파싱
+- (void) parseUpdateComment:(NSString *)jsonString {
+    NSLog(@"BIDLibInfoViewController - parseUpdateComment 메서드 실행");
+    
+    SBJsonParser* parser = [ [ SBJsonParser alloc ] init ];
+    
+    NSMutableDictionary* updateComment = [ parser objectWithString: jsonString ];    
+    
+    NSLog(@"요청처리시간: %@", [updateComment valueForKey:@"time"]);
+    NSLog(@"댓글 업데이트 결과: %@", [updateComment valueForKey:@"result"]);   
+    
+    [self viewDidAppear:NO];
+}
+
+
 
 
 //HTTP통신 완료되었을 경우 실행되는 메서드(데이터를 받아왔을 때)
@@ -146,22 +254,126 @@ NSString *libInfoDataFlag = nil; //어떤 데이터를 받아온건지 구분해
 //        [self parseDist:jsonString];
     }
     else if ([libInfoDataFlag isEqualToString:@"getComment"]) {
-        //        [self parseComment:jsonString];
+        [self parseComment:jsonString];
     }
     else if ([libInfoDataFlag isEqualToString:@"updateComment"]) {
-        //        [self parseUpdateComment:jsonString];
+        [self parseUpdateComment:jsonString];
     }
     else if ([libInfoDataFlag isEqualToString:@"getRating"]) {
         [self parseRating:jsonString];
     }
     else if ([libInfoDataFlag isEqualToString:@"updateRating"]) {
-        //        [self parseUpdateRating:jsonString];
+        [self parseUpdateRating:jsonString];
+
     }
     else {
         
     }
 }
 
+//HTTP통신 완료되었을 경우 실행되는 메서드(데이터를 받아왔을 때)
+- (void)didReceiveFinished2:(NSString *)result
+{
+    NSLog(@"didReceiveFinished2 메소드 실행. dataFlag : %@", libInfoDataFlag2);
+    
+    NSString *jsonString = result;
+    
+    //dataFlag 값에 따라서 각각 다른 파싱메서드를 호출해준다
+    if ([libInfoDataFlag2 isEqualToString:@"getRadius"]) {
+        //        [self parseRadius:jsonString];
+    }
+    else if ([libInfoDataFlag2 isEqualToString:@"getDist"]) {
+        //        [self parseDist:jsonString];
+    }
+    else if ([libInfoDataFlag2 isEqualToString:@"getComment"]) {
+        [self parseComment:jsonString];
+    }
+    else if ([libInfoDataFlag2 isEqualToString:@"updateComment"]) {
+        [self parseUpdateComment:jsonString];
+    }
+    else if ([libInfoDataFlag2 isEqualToString:@"getRating"]) {
+        [self parseRating:jsonString];
+    }
+    else if ([libInfoDataFlag2 isEqualToString:@"updateRating"]) {
+        [self parseUpdateRating:jsonString];
+        
+    }
+    else {
+        
+    }
+}
+
+
+- (IBAction)rate:(id)sender
+{
+    [self updateRating:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_class"] idx:[[NSUserDefaults standardUserDefaults] stringForKey:@"currentLibInfo_id"] rating:ratingText.text uuid:[[NSUserDefaults standardUserDefaults] stringForKey:@"uuid"] ];
+}
+
+
+//평점 업데이트. POST
+- (void) updateRating:(NSString *)library idx:(NSString *)idx rating:(NSString *)rating uuid:(NSString *)uuid {
+    NSLog(@"updateRating 메서드 실행");
+    
+    libInfoDataFlag = @"updateRating";
+    
+    //접속할 주소 설정
+    NSString *url = [[NSString alloc] initWithFormat:@"http://seoullibrary.herokuapp.com/rating/update"];
+    
+    // HTTP Request 인스턴스 생성
+    HTTPRequestPost *httpRequestPost = [[HTTPRequestPost alloc] init];
+    
+    // POST로 전송할 데이터 설정
+    NSDictionary *bodyObject = [NSDictionary dictionaryWithObjectsAndKeys:library,@"library",idx, @"idx", rating, @"rating", uuid, @"uuid", nil];
+    
+    // 통신 완료 후 호출할 델리게이트 셀렉터 설정
+    [httpRequestPost setDelegate:self selector:@selector(didReceiveFinished:)];
+    
+    // 페이지 호출
+    [httpRequestPost requestUrl:url bodyObject:bodyObject];
+}
+
+
+//평점 업데이트 후 받아온 데이터 파싱
+- (void) parseUpdateRating:(NSString *)jsonString {
+    NSLog(@"parseUpdateRating 메서드 실행");
+    SBJsonParser* parser = [ [ SBJsonParser alloc ] init ];
+    
+    NSMutableDictionary* ratingResult = [ parser objectWithString: jsonString ];    
+    
+    NSLog(@"요청처리시간: %@", [ratingResult valueForKey:@"time"]);
+    NSLog(@"평점 업데이트 결과: %@", [ratingResult valueForKey:@"result"]);    
+    [self viewDidAppear:NO];
+}
+
+
+// =====================테이블 뷰 세팅 관련 메서드 시작===========================
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5; //다시 설정해야함. 임의로 5개로 잡아둠.
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cIdentifier = @"cell"; //셀 구분자 정의
+    UITableViewCell *cell = [commentTable dequeueReusableCellWithIdentifier:cIdentifier]; //식별자와 함꼐 재사용가능한 셀 만들기
+    if(cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cIdentifier];
+    }
+    
+    for (int i=0; i < [[NSUserDefaults standardUserDefaults] integerForKey:@"commentCount"]; i++) {
+        cell.textLabel.text = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"comment%i_article", i]];
+        cell.detailTextLabel.text = [[NSUserDefaults standardUserDefaults] stringForKey:[NSString stringWithFormat:@"comment%i_date", i]];
+    }
+    
+    return cell;
+}
+
+
+// =====================테이블 뷰 세팅 관련 메서드 끝===========================
 
 
 
